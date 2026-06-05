1f1296201e778c5b97066dd63304b0d373834ae0
jthoms29 <johnthoms567@gmail.com>
2026-06-05 10:59:58 -0600

    sorting functionality for diff fields

--- src/lib_mem.c
+++ src/lib_mem.c
@@ -1,5 +1,9 @@
 #include <music_player2.h>
 
+int artist_compare(const void* a1, const void* a2);
+int album_compare(const void* a1, const void* a2);
+int song_compare(const void* s1, const void* s2);
+
 lib_mem* lib_mem_init(void) {
     lib_mem* lib = malloc(sizeof(*lib));
     if (!lib) {
@@ -7,9 +11,9 @@ lib_mem* lib_mem_init(void) {
         return NULL;
     }
 
-    JVEC* artists = JVEC_new(free, string_compare);
-    JVEC* albums = JVEC_new(free, string_compare);
-    JVEC* songs = JVEC_new(free, string_compare);
+    JVEC* artists = JVEC_new(NULL, artist_compare);
+    JVEC* albums = JVEC_new(NULL, album_compare);
+    JVEC* songs = JVEC_new(NULL, song_compare);
 
     return lib;
 }
@@ -30,13 +34,15 @@ int load_artists(lib_mem* mem, lib_db* db) {
         id = sqlite3_column_int(pstmt, 0);
         name = (char*) sqlite3_column_text(pstmt, 1);
         artist* atst = calloc(1, sizeof(*atst));
+        JVEC* albums = JVEC_new(NULL, string_compare);
+        atst->albums = albums;
+
         char* name_alloc = malloc(strlen(name) + 1);
 
         strcpy(name_alloc, name);
         atst->name = name_alloc;
 
         JVEC_append(vec, atst);
-
     }
 
     JVEC_sort(vec);
@@ -45,4 +51,25 @@ int load_artists(lib_mem* mem, lib_db* db) {
 // load persistent library stored in sql database into memory
 int load_library(lib_mem* mem, lib_db* db) {
 
+}
+
+
+int artist_compare(const void* a1, const void* a2) {
+    artist* _a1 = (artist*) a1;
+    artist* _a2 = (artist*) a2;
+    return strcmp(_a1->name, _a2->name);
+}
+
+int album_compare(const void* a1, const void* a2) {
+    album* _a1 = (album*) a1;
+    album* _a2 = (album*) a2;
+
+    return _a2->original_year - _a1->original_year;
+}
+
+int song_compare(const void* s1, const void* s2) {
+    song* _s1 = (song*) s1;
+    song* _s2 = (song*) s2;
+
+    return _s2->track_num - _s1->track_num;
 }
