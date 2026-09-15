95dc10de5bf09322a875fa77095c98d2f68af4ba
jthoms29 <johnthoms567@gmail.com>
2026-09-15 16:46:47 -0600

    cleaned up audio_playback implementation

--- src/audio_playback.c
+++ src/audio_playback.c
@@ -5,60 +5,81 @@
 #include <audio_playback.h>
 
 void end_callback(void* pUserData, ma_sound* pSound) {
-    // JUST NOTIFY
-    audio_player* ap = pUserData;
-    ap->playing = false;
-    ap->ready = true;
+
+    // signal poll somehow
 }
 
-audio_player* audio_new() {
 
+audio_player* audio_new() {
     audio_player* ap = calloc(1, sizeof(*ap));
     if (!ap) {
         perror("audio_new(): failed to alloc new audio_player struct");
         return NULL;
     }
-
     ma_result result;
-
     result = ma_engine_init(NULL, &ap->engine);
     if (result != MA_SUCCESS) {
+        fprintf(stderr, "audio_new(): failed to init audio engine");
         free(ap);
         return NULL;
     }
-
-    ap->ready = false;
-
     return ap;
+}
+
+void audio_free(audio_player** ap_ptr) {
+    if (!(*ap_ptr)) { return; }
+
+    // pretty sure it's safe to pass NULL to these, don't need check
+    ma_engine_uninit(&(*ap_ptr)->engine); // :)
+    ma_sound_uninit(&(*ap_ptr)->sound);
+    
+    // free the ap struct itself
+    free(*ap_ptr);
+    *ap_ptr = NULL;
 }
 
 
 
 int audio_load_song(audio_player* ap, song* sng) {
-
     ma_result res;
-    if (!ma_sound_is_playing(&ap->sound)) {
+
+    // if song is currently playing, stop it
+    if (ma_sound_is_playing(&ap->sound)) {
         ma_sound_uninit(&ap->sound);
     }
 
+    // load new song
     res = ma_sound_init_from_file(&ap->engine, sng->path, 0, NULL, NULL, &ap->sound);
     if (res != MA_SUCCESS) {
         ma_sound_uninit(&ap->sound);
         return -1;
     }
+    // set function sound will call when ends
     ma_sound_set_end_callback(&ap->sound, end_callback, ap);
+
+    // start playing song in miniaudio thread
     ma_sound_start(&ap->sound);
-    ap->playing = true;
-    ap->ready = false;
+    return 0;
+}
+
+bool audio_is_playing(audio_player* ap) {
+    return ma_sound_is_playing(&ap->sound);
+}
+
+int audio_pause(audio_player* ap) {
+    if (!ma_sound_is_playing(&ap->sound)) {
+        return 0;
+    }
+    ma_sound_stop(&ap->sound);
     return 0;
 }
 
 double audio_current_pos(audio_player* ap) {
-    if (!ap->playing) {
+    // song is not currently playing
+    if (!ma_sound_is_playing(&ap->sound)) {
         return 0;
     }
 
-    float pos;
-    ma_sound_get_cursor_in_seconds(&ap->sound, &pos);
+    float pos = ma_sound_get_cursor_in_seconds(&ap->sound, &pos);
     return (double) pos;
 }
--- src/audio_playback.h
+++ src/audio_playback.h
@@ -8,17 +8,66 @@
 typedef struct audio_player {
     ma_engine engine;
     ma_sound sound;
-
-    bool playing;
-
-    bool ready;
-
 } audio_player;
 
+/**
+ * @brief Create and return a new audio_player struct
+ * @details Allocates a new audio_player struct, initializes miniaudio engine
+ * @note Must be freed with `audio_free()`
+ * @pre None 
+ * @post Heap allocated audio_player struct returned, miniaudio engine within it initialized
+ * @return Pointer to struct on success, NULL on failure
+ */
 audio_player* audio_new();
 
+/**
+ * @brief Frees audio_player struct
+ * @details Frees audio_player struct, unitializes all miniaudio components stored within
+ * @param[in, out] ap_ptr Indirect pointer to audio_player struct
+ * @pre struct must be null or been allocated with `audio_new()`
+ * @post struct freed, all miniaudio components within unitialized, struct set to NULL
+ * @return None
+ */
+void audio_free(audio_player** ap_ptr);
 
+/**
+ * @brief Load and start playing a new song
+ * @details Halts playback on current song if playing, loads song specified in sng's `path` member
+ * @param[in, out] ap pointer to audio_player struct created by `audio_new()`
+ * @param[in] sng song struct retrieved from in-memory music library
+ * @pre ap must be non-NULL, created with `audio_new()`, sng must be non-NULL
+ * @post Current song halted if playing, song specified by sng is loaded and started
+ * @return 0 on success, anything else on failure
+ */
+int audio_load_song(audio_player* ap, song* sng);
 
+/**
+ * @brief Retrieves the time of current song's playback in seconds
+ * @param[in] player pointer to audio_player created by `audio_new()`
+ * @pre player must have been created with `audio_new()`
+ * @post Time of currently playing song returned
+ * @return Time in seconds of currently playing song, 0 if no song currently playing
+ */
 double audio_current_pos(audio_player *player);
-int audio_load_song(audio_player* ap, song* song);
+
+/**
+ * @brief Returns if song is currently playing
+ * @param[in] ap pointer to audio_player struct
+ * @pre ap must have been created with `audio_new()`
+ * @post current state of playing audio returned
+ * @return true if song is currently playing, false otherwise
+ */
+bool audio_is_playing(audio_player* ap);
+
+/**
+ * @brief Pause the currently playing song
+ * @details Pause playback of currently selected song within ap
+ * @param[in, out] ap pointer to ap audio_player struct
+ * @pre ap must have been created with `audio_new()`
+ * @post if song is currently playing, it is paused
+ * @return 0 on success, anything else on failure
+ */
+int audio_pause(audio_player* ap);
+
+
 #endif
